527e4c588e29e87f59a903a9048abc0084fe8226
Ryan Howard <ryanhoward810@gmail.com>
2017-11-21 15:00:26 -0800

    committing files

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Ryan Howard',
+    profilePic: 'https://avatars1.githubusercontent.com/u/30733851?s=400&u=b66428294103e739fadb426d418353b78b081b91&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
